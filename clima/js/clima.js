3d381a63652bb7499a4ba6aa0d1d4fc06ccfae26
Matias-M-ETN26 <matiasmonzonetn26@gmail.com>
2022-10-26 15:50:11 -0300

    clima finalizado

--- clima/js/clima.js
+++ clima/js/clima.js
@@ -1,93 +1,89 @@
 function clima(dia) {
-    console.log(dia);
+    $('#dia-seleccionado').hide();
+    $('#dias-contenedor').hide();
+    $('#cargando').show();
     var latitud = -34.6140489;
     var longitud = -58.4078955;
     const apiPersonal = "8ead1a015aabf195b5e511574525951c"
-    $.ajax({
-        url: 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitud + '&lon=' + longitud + '&appid=' + apiPersonal + '&units=metric&lang=sp',
-        dataType: 'json',
-        success: function (info) {
-            console.log(info)
-            /* Declaro variables */
-            //HTML
-            var img = $("#img-seleccionado");
-            var temp_promedio = $("#temp-promedio");
-            var precipitaciones = $("#precipitaciones");
-            var humedad = $("#humedad");
-            var viento = $("#viento");
-            var localidad = $("#localidad");
-            var dia_semana = $("#dia-semana");
-            var clima_info = $("#clima-info");
-            var dia_contenedor = $("#dias-contenedor");
-            var diaActual = new Date();
-            var diaActual = diaActual.getDay();
-            var diasContenido = "";
-            var regisDiaSig = "";
-            // Array
-            var diaSelecionado = [];
-            const arrayDias = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
-            /* Promedios */
-            var temp_promedioRes = 0;
-            var precipitacion_promedio = 0;
-            var humedad_promedio = 0;
-            var viento_promedioMs = 0;
-            var viento_promedioKms = 0;
-            /* Averiguo los datos de hoy */
-            var diaVariable = parseInt(info["list"][0]["dt_txt"].split(" ")[0].split("-")[2]) + dia;
-            console.log(diaVariable)
-            var cantRegDiavariable = 0;
-            info["list"].forEach((elemento, index) => {
-                if (info["list"][index]["dt_txt"].split(" ")[0].split("-")[2] == diaVariable) {
-                    cantRegDiavariable++;
-                }else if(info["list"][index]["dt_txt"].split(" ")[0].split("-")[2] !== diaVariable && info["list"][index]["dt_txt"].split(" ")[0].split("-")[2] - diaVariable == 1 && regisDiaSig == ""){
-                    regisDiaSig = index;
+        $.ajax({
+            url: 'https://api.openweathermap.org/data/2.5/forecast?lat=' + latitud + '&lon=' + longitud + '&appid=' + apiPersonal + '&units=metric&lang=sp',
+            dataType: 'json',
+            success: function (info) {
+                console.log(info)
+                /* Declaro variables */
+                //HTML
+                var img = $("#img-seleccionado");
+                var temp_promedio = $("#temp-promedio");
+                var celsius = $("#celsius");
+                var precipitaciones = $("#precipitaciones");
+                var humedad = $("#humedad");
+                var viento = $("#viento");
+                var localidad = $("#localidad");
+                var dia_semana = $("#dia-semana");
+                var clima_info = $("#clima-info");
+                var dia_contenedor = $("#dias-contenedor");
+                var diaActual = (new Date()).getDay();
+                var diasContenido = "";
+                // Array
+                const arrayDias = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+                /* Promedios */
+                var temp_promedioRes = 0;
+                var precipitacion_promedio = 0;
+                var humedad_promedio = 0;
+                var viento_promedioMs = 0;
+                var viento_promedioKms = 0;
+                /* Averiguo los datos de hoy */
+
+                var diaVariable = parseInt(info["list"][0]["dt_txt"].split(" ")[0].split("-")[2]) + dia;
+                var diaVariableRegis = [];
+                info["list"].forEach((elemento, index) => {
+                    if (info["list"][index]["dt_txt"].split(" ")[0].split("-")[2] == diaVariable) {
+                        diaVariableRegis.push(index);
+                    }
+                });
+                var cantRegDiavariable = diaVariableRegis.length;
+                /* Promedio Datos */
+                for (i = 0; i < cantRegDiavariable; i++) {
+                    temp_promedioRes += Math.round((info["list"][diaVariableRegis[i]]["main"]["temp_max"] + info["list"][diaVariableRegis[i]]["main"]["temp_min"]) / 2);
+                    precipitacion_promedio += info["list"][diaVariableRegis[i]]["pop"];
+                    humedad_promedio += info["list"][diaVariableRegis[i]]["main"]["humidity"];
+                    viento_promedioMs += info["list"][diaVariableRegis[i]]["wind"]["speed"];
+                    viento_promedioKms += (info["list"][diaVariableRegis[i]]["wind"]["speed"]) * (18 / 5);
                 }
-            });
-            for(i=0;i<5;i++){
+                temp_promedioRes = Math.round(temp_promedioRes / cantRegDiavariable); //Promedio de la temperatura de hoy
+                precipitacion_promedio = Math.round((precipitacion_promedio / cantRegDiavariable) * 100); //Promedio de la prob. de precipitaciones de hoy
+                humedad_promedio = Math.round(humedad_promedio / cantRegDiavariable); //Promedio de la humedad de hoy
+                viento_promedioMs = Math.round(viento_promedioMs / cantRegDiavariable); //Promedio del viento en m/s de hoy
+                viento_promedioKms = Math.round(viento_promedioKms / cantRegDiavariable); //Promedio del viento en km/h de hoy
 
+                /* Reemplazo datos en HTML*/
+                /* Datos en dia-seleccionado-contenedor */
+                img.html(`<img class="img-clima" src="http://openweathermap.org/img/wn/${info["list"][diaVariableRegis[0]]["weather"][0]["icon"]}@2x.png"></img>`);
+                temp_promedio.html(temp_promedioRes);
+                celsius.html("°C");
+                precipitaciones.html(`Prob. de precipitaciones: ${precipitacion_promedio}%`);
+                humedad.html(`Humedad: ${humedad_promedio}%`);
+                viento.html(`Viento: a ${viento_promedioMs} m/s o ${viento_promedioKms} km/h`);
+                localidad.html(info["city"]["name"]);
+                dia_semana.html(arrayDias[diaActual]);
+                clima_info.html(info["list"][diaVariableRegis[0]]["weather"][0]["description"][0].toUpperCase() + info["list"][diaVariableRegis[0]]["weather"][0]["description"].substring(1));
+                /* Datos en dia-contenedor */
+                for (i = 0; i < 4; i++) {
+                    diasContenido += '<div class="dias-contenedor" id="dias-contenedor">';
+                    diasContenido += `<a href="javascript:clima(${i});">`;
+                    diasContenido += `<div class="dias ${i == dia ? "seleccionado" : ""}">`;
+                    diasContenido += `<div class="fecha">${arrayDias[diaActual]}</div>`;
+                    diasContenido += `</div>`;
+                    diasContenido += `</a>`;
+                    diasContenido += `</div>`;
+                    diaActual = (diaActual + 1) !== 7 ? diaActual + 1 : 0;
+                }
+                dia_contenedor.html(diasContenido);
+                $('#dia-seleccionado').show();
+                $('#dias-contenedor').show();
+                $('#cargando').hide();
             }
-            
-            /* Promedio Datos */
-            for (i = 0; i < cantRegDiavariable; i++) {
-                temp_promedioRes += Math.round((info["list"][i]["main"]["temp_max"] + info["list"][i]["main"]["temp_min"]) / 2);
-                precipitacion_promedio += info["list"][i]["pop"];
-                humedad_promedio += info["list"][i]["main"]["humidity"];
-                viento_promedioMs += info["list"][i]["wind"]["speed"];
-                viento_promedioKms += (info["list"][i]["wind"]["speed"]) * (18 / 5);
-            }
-            temp_promedioRes = Math.round(temp_promedioRes / cantRegDiavariable); //Promedio de la temperatura de hoy
-            precipitacion_promedio = precipitacion_promedio / cantRegDiavariable; //Promedio de la prob. de precipitaciones de hoy
-            humedad_promedio = Math.round(humedad_promedio / cantRegDiavariable); //Promedio de la humedad de hoy
-            viento_promedioMs = Math.round(viento_promedioMs / cantRegDiavariable); //Promedio del viento en m/s de hoy
-            viento_promedioKms = Math.round(viento_promedioKms / cantRegDiavariable); //Promedio del viento en km/h de hoy
+        });
 
-            /* Reemplazo datos en HTML*/
-            /* Datos en dia-seleccionado-contenedor */
-            img.html(`<img class="img-clima" src="http://openweathermap.org/img/wn/${info["list"][0]["weather"][0]["icon"]}@2x.png"></img>`);
-            temp_promedio.html(temp_promedioRes);
-            precipitaciones.html(`Prob. de precipitaciones: ${precipitacion_promedio}%`);
-            humedad.html(`Humedad: ${humedad_promedio}%`);
-            viento.html(`Viento: a ${viento_promedioMs} m/s o ${viento_promedioKms} km/h`);
-            localidad.html(info["city"]["name"]);
-            dia_semana.html(arrayDias[diaActual]);
-            clima_info.html(info["list"][0]["weather"][0]["description"][0].toUpperCase() + info["list"][0]["weather"][0]["description"].substring(1));
-            /* Datos en dia-contenedor */
-            for (i = 0; i < 5; i++) {
-                diasContenido += '<div class="dias-contenedor" id="dias-contenedor">';
-                diasContenido += `<a href="javascript:clima(${i});">`;
-                diasContenido += `<div class="dias ${i == dia ? "seleccionado" : ""}">`;
-                diasContenido += `<div class="fecha">${arrayDias[diaActual]}</div>`;
-                diasContenido += `<img src="//ssl.gstatic.com/onebox/weather/64/partly_cloudy.png">`;
-                diasContenido += `<div class="temp">`;
-                diasContenido += `<div class="temp-max">${info["list"][regisDiaSig]["main"]["m"]}</div>`;
-                diasContenido += `<div class="temp-min">18°</div>`;
-                diasContenido += `</div>`;
-                diasContenido += `</div>`;
-                diasContenido += `</a>`;
-                diasContenido += `</div>`;
-                diaActual = (diaActual + 1) !== 7 ? diaActual + 1: 0;
-            }
-            dia_contenedor.html(diasContenido);
-        }
-    });
+
 }
